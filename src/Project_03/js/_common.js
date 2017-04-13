70b18f8449158739a32f6ab0b64fef6832490cad
baekjinyeong <jinyeong1612@cttd.co.kr>
2017-04-13 18:01:34 +0900

    js작업

--- src/Project_03/js/_common.js
+++ src/Project_03/js/_common.js
@@ -3,5 +3,15 @@
 /*! _common.js */
 (function () {
 	'use strict';
-	console.log('_common.js');
+	// console.log('_common.js');
+	var i = 0;
+	$('.header_main_nav').hover(
+		function(){
+			$('.header_main_nav_item ul').fadeIn('normal',function(){$(this).stop();});
+			$('.header_main_nav').animate({height:'170px'},300).clearQueue();
+		},
+		function(){
+			$('.header_main_nav_item ul').fadeOut('fast');
+			$('.header_main_nav').animate({height:'35px'},300).clearQueue();
+	});
 }());
