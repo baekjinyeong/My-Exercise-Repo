7b6a3487081ec6f39245696a4d3ace6504280dac
baekjinyeong <jinyeong1612@cttd.co.kr>
2017-06-09 15:10:24 +0900

    cj 작업 중

--- src/Project_03/js/_common.js
+++ src/Project_03/js/_common.js
@@ -1,17 +1,16 @@
 //= require ../_vendor/library.js
 
 /*! _common.js */
-(function () {
-	'use strict';
-	// console.log('_common.js');
-	var i = 0;
-	$('.header_main_nav').hover(
-		function(){
-			$('.header_main_nav_item ul').fadeIn('normal',function(){$(this).stop();});
-			$('.header_main_nav').animate({height:'170px'},300).clearQueue();
-		},
-		function(){
-			$('.header_main_nav_item ul').fadeOut('fast');
-			$('.header_main_nav').animate({height:'35px'},300).clearQueue();
-	});
-}());
+// (function () {
+// 	'use strict';
+// 	// console.log('_common.js');
+// 	$('.header_main_nav_all').hover(
+// 		function(){
+// 			// $('.header_main_nav_list li').fadeIn('normal',function(){$(this).stop();});
+// 			$('.header_main_nav').animate({height:'170px'},300).clearQueue();
+// 		},
+// 		function(){
+// 			// $('.header_main_nav_item').fadeOut('fast');
+// 			// $('.header_main_nav').animate({height:'35px'},300).clearQueue();
+// 	});
+// }());
