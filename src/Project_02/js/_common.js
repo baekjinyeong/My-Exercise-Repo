4c340a7ac2e2d4b9e7927fa470db009b96756a06
baekjinyeong <jinyeong1612@cttd.co.kr>
2017-06-21 18:05:32 +0900

    js작업

--- src/Project_02/js/_common.js
+++ src/Project_02/js/_common.js
@@ -2,7 +2,36 @@
 
 /*! _common.js */
 (function () {
-	'use strict';
 
-	console.log('_common.js');
+	// 변수를 선언한다.
+	// li의 인덱스 번호를 찾는다.
+	// 버튼을 활성화 시킨다.
+	// li의 인덱스 번호로 화살표 버튼 클릭 시 화면을 전환시킨다.
+
+	// var slideIndex = 1; // 변수 slideIndex = 1을 선언한다.
+	// showDivs(slideIndex); // showDivs안에 slideIndex 를 담는다.
+	//
+	// function plusDivs(n){// 함수 plusDivs(n) 을 선언한다.
+	// 	showDivs(slideIndex += n);
+	// }
+	//
+	// function circleDivs(n) {
+	// 	showDivs(slideIndex += n);
+	// }
+	//
+	// function showDivs(n) {
+	// 	var x = document.getElementsByClassName('box'); // li를 가져온다.
+	//
+	// 	if (n > x.length) { // 만약 n 이 li의 길이보다 크다면...
+	// 		slideIndex = 1; // slideIndex 는 1이다.
+	// 	}else if (n < 1) { // 그렇지 않고 n 이 1보다 작으면..
+	// 		slideIndex = x.length; // slideIndex 는 li의 길이와 같다.
+	// 	}
+	//
+	// 	for (var i = 0; i < x.length; i++){ // i는 0이고 i가 li의 길이보다 작을 때 i를 증가시킨다.
+	// 		x[i].style.display = 'none'; // li의 인덱스 값을 찾아서 display = none; 시킨다.
+	// 		console.log(x[i]);
+	// 	}
+	// 	x[slideIndex-1].style.display = 'block';
+	// }
 }());
